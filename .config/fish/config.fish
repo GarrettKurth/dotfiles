2c145c3cc836cf54a2a386a09b8533c29ef4d9ac
Garrett <garrettkurth@gmail.com>
2024-06-03 11:23:26 -0500

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+starship init fish | source
+fastfetch
+alias eac3to="wine /home/garrett/Remuxing/Tools/eac3to/eac3to.exe"
+alias v="nvim"
+alias pftp="lftp polar"
+end
---
+++ .wezterm.lua
@@ -0,0 +1,37 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+
+config.enable_tab_bar = false
+config.window_background_opacity = 0.8
+config.color_scheme = 'catppuccin-macchiato'
+
+config.font = wezterm.font {
+  family = 'FiraCode Nerd Font',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+}
+
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane{confirm=true},
+  },
+
+	  
+}
+
+
+
+-- The filled in variant of the < symbol
+
+-- and finally, return the configuration to wezterm
+return config
