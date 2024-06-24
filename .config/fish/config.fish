ecf274caa7d443a02287508f47519cabfa1fb023
Garrett <garrettkurth@gmail.com>
2024-06-24 14:48:03 -0500

    smile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 starship init fish | source
+
 fastfetch
-alias eac3to="wine /home/garrett/Remuxing/Tools/eac3to/eac3to.exe"
+alias eac3to="wine /home/sol/Remuxing/Tools/eac3to/eac3to.exe"
 alias v="nvim"
 alias pftp="lftp polar"
+alias x="xsel -ib"
 end
