f4f971c9a2bb760cb2e1ffd96049c0ee0b75a9c1
Garrett <garrettkurth@gmail.com>
2024-08-30 14:33:50 -0500

    fish adjustments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,7 @@ if status is-interactive
 starship init fish | source
 
 fastfetch
-alias eac3to="wine /home/sol/Remuxing/Tools/eac3to/eac3to.exe"
 alias v="nvim"
-alias pftp="lftp polar"
 alias x="xsel -ib"
 enable_transience
-tmux
 end
