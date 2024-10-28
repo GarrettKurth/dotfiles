febeb396ff26c23aaec00a8ca2c29f89b17ecf76
Garrett Kurth <garrettkurth@gmail.com>
2024-10-28 18:34:51 -0500

    changed cmp autocomplete to tab

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = {
+    -- Add this line to confirm selection with Tab as well
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+  },
+  -- other configuration options...
+})
