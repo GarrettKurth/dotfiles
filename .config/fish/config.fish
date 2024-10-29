b73500956f4e8d0f17527cd2e09d3ea67bf1cd94
Garrett Kurth <garrettkurth@gmail.com>
2024-10-28 19:40:51 -0500

    fixed starship bc I didnt add the theme to the config & added clear to start of fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-starship init fish | source
-
-fastfetch
-alias v="nvim"
-alias x="xsel -ib"
-enable_transience
-fish_ssh_agent
-if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
+    starship init fish | source
+    clear
+    fastfetch
+    alias v="nvim"
+    alias x="xsel -ib"
+    enable_transience
+    fish_ssh_agent
+    if status is-interactive
+        and not set -q TMUX
+        exec tmux
+    end
 end
